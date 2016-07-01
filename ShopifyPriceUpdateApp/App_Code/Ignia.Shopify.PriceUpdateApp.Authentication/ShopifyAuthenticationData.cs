a33c337f9327795b199cfc1b72b1ba4e05ec95a0
Katherine Trunkey <ktrunkey@gmail.com>
2016-07-01 07:59:57 -0700

    Scaffolded RestSharp authentication classes

---
+++ ShopifyPriceUpdateApp/App_Code/Ignia.Shopify.PriceUpdateApp.Authentication/ShopifyAuthenticationData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ignia.Shopify.PriceUpdateApp.Authentication {
+
+  /*============================================================================================================================
+  | CLASS: SHOPIFY AUTHENTICATON DATA
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Container object for authentication properties returned as part of the Shopify OAuth authentication response.
+  /// </summary>
+  public class ShopifyAuthenticationData {
+
+    /// <summary>
+    ///   The authorization code with which to make authenticated Shopify requests.
+    /// </summary>
+    public string Code {
+      get; set;
+    }
+
+    /// <summary>
+    ///   The hash-based authorization code passed through the authentication response; must be validated.
+    /// </summary>
+    public string Hmac {
+      get; set;
+    }
+
+    /// <summary>
+    ///   The fully-qualified Shopify shop subdomain.
+    /// </summary>
+    public string Shop {
+      get; set;
+    }
+
+    /// <summary>
+    ///   The unique identifier sent with the request; used to validate the response.
+    /// </summary>
+    public string State {
+      get; set;
+    }
+
+    /// <summary>
+    ///   Timestamp sent with the request response; used for response verification.
+    /// </summary>
+    public string Timestamp {
+      get; set;
+    }
+
+  }
+
+}
---
+++ ShopifyPriceUpdateApp/App_Code/Ignia.Shopify.PriceUpdateApp.Authentication/ShopifyAuthenticator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace Ignia.Shopify.PriceUpdateApp {
+
+  /*============================================================================================================================
+  | CLASS: SHOPIFY AUTHENTICATOR
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Extends the global <see cref="Startup" /> class by providing methods it can use to configure authorization.
+  /// </summary>
+  ///  <param name="accessToken">The Shopify application's access token (API key).</param>
+  public class ShopifyAuthenticator : OAuth2Authenticator {
+    public ShopifyAuthenticator(string accessToken) : base(accessToken) {
+    }
+
+    /*==========================================================================================================================
+    | AUTHENTICATE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Override for RestSharp's <see 
+    ///   cref="RestSharp.Authenticators.OAuth2Authenticator.Authenticate(IRestClient, IRestRequest)" />
+    ///   method; adds Shopify's Access Token header to the authorization request, if it has not already been added.
+    /// </summary>
+    /// <param name="client">The REST client object.</param>
+    /// <param name="request">The REST request.</param>
+    public override void Authenticate(IRestClient client, IRestRequest request) {
+      if (!request.Parameters.Any(p => p.Name.Equals("X-Shopify-Access-Token", StringComparison.OrdinalIgnoreCase))) {
+        request.AddParameter("X-Shopify-Access-Token", AccessToken, ParameterType.HttpHeader);
+      }
+    }
+
+  }
+}
